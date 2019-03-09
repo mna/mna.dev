97a9d4eca4701c781f45f60696c9c1530abe690c
Martin Angers <martin.n.angers@gmail.com>
2019-03-08 23:07:33 -0500

    generate basic template with common head elements

--- scripts/build-templates/main.go
+++ scripts/build-templates/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"text/template"
 )
 
 type website struct {
@@ -15,5 +16,26 @@ func main() {
 	}
 
 	posts, src, dst := os.Args[1], os.Args[2], os.Args[3]
-	fmt.Println(">>>> ", posts, src, dst)
+	t, err := template.ParseGlob(filepath.Join(src, "*"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := execute(t, "index.html", dst); err != nil {
+		log.Fatal(err)
+	}
+	_ = posts
+}
+
+func execute(t *template.Template, name string, outDir string) error {
+	f, err := os.Create(filepath.Join(outDir, name))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if err := t.ExecuteTemplate(f, name, nil); err != nil {
+		return err
+	}
+	return f.Close()
 }
