b3e8d47228697796182ad483cbcf01a6ed2b9059
Martin Angers <martin.n.angers@gmail.com>
2019-03-16 14:42:28 -0400

    filter on multiple tags, search in filtered cards

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,5 +1,12 @@
-let allCards = []
+;(function() {
 
+let allCards = []
+let haystackCards = []
+let allTags = []
+let selectedTags = new Set()
+let searchBox = null
+
+// watch for document ready and call cb when it is.
 function ready(cb) {
   // in case the document is already rendered
   if (document.readyState != "loading") {
@@ -9,6 +16,8 @@ function ready(cb) {
   }
 }
 
+// returns a function that calls fn only after delay milliseconds
+// have passed without other calls.
 function debounced(delay, fn) {
   let timerId
   return function (...args) {
@@ -22,14 +31,22 @@ function debounced(delay, fn) {
   }
 }
 
+// resets the search box.
+function clearSearch() {
+  searchBox.value = ""
+  // TODO: clear query string or whatever is used to link to a search
+}
+
 // TODO: https://developer.mozilla.org/en-US/docs/Web/API/History_API
 // set the filter as a query string and auto-filter on page load.
 
+// show only cards from the haystack that match the searched words.
 function filterCardsBySearch(e) {
   let text = e.target.value.trim().toLowerCase()
   let words = text.match(/\S+/g) || []
 
-  allCards.forEach(card => {
+  // search only in the haystack, i.e. in the currently selected tag(s)
+  haystackCards.forEach(card => {
     let content = card.innerText
     let tweet = card.querySelector(".twitter-tweet")
     if (tweet) {
@@ -51,35 +68,67 @@ function filterCardsBySearch(e) {
   })
 }
 
-function filterCardsByTag(e) {
-  let tag = e.target.innerText
+function isSuperset(set, subset) {
+  for (var elem of subset) {
+    if (!set.has(elem)) {
+      return false
+    }
+  }
+  return true
+}
+
+function tagClicked(e) {
+  e.preventDefault()
+
+  // filtering on a tag clears the search and resets the haystack
+  clearSearch()
+  haystackCards.length = 0
+
+  let selectedTag = e.target.innerText
+  if (selectedTags.has(selectedTag)) {
+    selectedTags.delete(selectedTag)
+  } else {
+    selectedTags.add(selectedTag)
+  }
+
+  allTags.forEach(tag => {
+    let tagText = tag.innerText
+    if (tagText === selectedTag) {
+      tag.classList.toggle("is-active")
+    }
+  })
 
   allCards.forEach(card => {
-    let cardTags = Array.from(card.querySelectorAll(".hashtag")).map(ht => ht.innerText)
+    let cardTags = new Set(Array.from(card.querySelectorAll(".hashtag")).map(ht => ht.innerText))
 
     let tweet = card.querySelector(".twitter-tweet")
     if (tweet) {
       // tweets have "#twitter" and "#micro" tags implied
-      cardTags = ["#twitter", "#micro"]
+      cardTags = new Set(["#twitter", "#micro"])
     }
 
-    if (cardTags.some(cardTag => cardTag === tag)) {
+    if (isSuperset(cardTags, selectedTags)) {
       card.classList.remove("hidden")
+      haystackCards.push(card)
     } else {
       card.classList.add("hidden")
     }
   })
 }
 
+// bootstrap execution, called when document is ready.
 ready(function() {
-  let tags = document.querySelectorAll(".hashtag")
-  tags.forEach(tag => {
-    tag.addEventListener("click", filterCardsByTag)
+  // grab list of all cards, and on load the haystack is the set of all cards
+  allCards = Array.from(document.querySelectorAll(".card"))
+  haystackCards = allCards.slice()
+
+  allTags = Array.from(document.querySelectorAll(".hashtag"))
+  allTags.forEach(tag => {
+    tag.addEventListener("click", tagClicked)
   })
 
-  // cannot extract tweets at this point, twitter script might not be loaded yet
-  allCards = Array.from(document.querySelectorAll(".card"))
-
-  let search = document.getElementById("search")
-  search.addEventListener("input", debounced(200, filterCardsBySearch))
+  searchBox = document.getElementById("search")
+  searchBox.addEventListener("input", debounced(200, filterCardsBySearch))
 })
+
+})();
